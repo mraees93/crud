dfa7a6449f350f707430f484bc245cfc337158a5
mraees93 <raeesdolley76@gmail.com>
2026-03-31 12:42:39 +0200

    added get all customers api

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace to_do_list.Controllers
 {
@@ -10,6 +11,46 @@ namespace to_do_list.Controllers
     [Route("api/[controller]")]
     public class CustomersController : ControllerBase
     {
-        
+        private readonly string _connectionString;
+
+        public CustomersController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection") ?? "";
+        }
+
+        [HttpGet] 
+        public IActionResult GetAll()
+        {
+            var customers = new List<object>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Customers ORDER BY id DESC";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            customers.Add(new {
+                                Id = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                Email = reader.GetString(3)
+                            });
+                        }
+                    }
+                }
+            }
+            return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
